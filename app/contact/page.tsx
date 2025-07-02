5d35f2d66d8af717d5a33ef9bb215e7b86ca8152
pranavvraja <pranavvraja@gmail.com>
2025-07-02 22:27:27 +0530

    add: contact us dialog box

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,21 @@
 "use client";
+
 import Navigation from "@/components/navigation";
 import Footer from "@/components/footer";
 import Image from "next/image";
-import { ArrowRight } from "lucide-react";
-import { motion } from "motion/react";
+import { ArrowRight, X } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function ContactPage() {
+  const [showDialog, setShowDialog] = useState(false);
+  const [tab, setTab] = useState("START A PROJECT");
+
+  const tabs = ["START A PROJECT", "WORK WITH US", "OTHER"];
+
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen w-full">
       <Navigation />
 
       {/* Contact Section */}
@@ -25,8 +33,13 @@ export default function ContactPage() {
               can we talk?
             </h1>
 
-            <button className="flex items-center border-b space-x-2 mb-16 text-black hover:opacity-70 transition-opacity">
-              <span className="text-xl  tracking-wider">CONTACT US</span>
+            <button
+              className="flex items-center border-b space-x-2 mb-16 text-black hover:opacity-70 transition-opacity"
+              onClick={() => setShowDialog(!showDialog)}
+            >
+              <span className="text-xl  tracking-wider cursor-pointer">
+                CONTACT US
+              </span>
               <ArrowRight className="w-4 h-4 ml-2" />
             </button>
           </motion.div>
@@ -36,7 +49,7 @@ export default function ContactPage() {
             transition={{ duration: 1.5, type: "decay" }}
             className="space-y-12 text-black flex flex-row justify-between"
           >
-            <div>
+            <div className="m-2">
               <div className="flex flex-col mb-8">
                 <h3 className="text-lg tracking-wider text-gray-600 mb-10">
                   CONTACT INFORMATION
@@ -91,6 +104,95 @@ export default function ContactPage() {
         {/* </div> */}
       </section>
 
+      <AnimatePresence>
+        {showDialog && (
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: "0%" }}
+            exit={{ x: "100%" }}
+            transition={{ duration: 0.6, ease: "easeInOut" }}
+            className="fixed top-0 right-0 h-full w-1/2 bg-stone-300 text-black z-50 shadow-xl px-12 py-16 overflow-y-auto"
+          >
+            <div className="flex justify-between items-center mb-10">
+              <h2 className="text-5xl font-medium">Can we talk?</h2>
+              <button
+                onClick={() => setShowDialog(false)}
+                className="cursor-pointer"
+              >
+                <X className="w-8 h-8 text-gray-600 hover:text-black" />
+              </button>
+            </div>
+
+            {/* Tabs */}
+            <div className="flex space-x-20 mb-20 text-md tracking-widest uppercase font-medium">
+              {tabs.map((t) => (
+                <button
+                  key={t}
+                  className={clsx(
+                    "pb-1 border-b transition",
+                    t === tab
+                      ? "border-black text-black"
+                      : "border-transparent text-gray-500 hover:text-black"
+                  )}
+                  onClick={() => setTab(t)}
+                >
+                  {t}
+                </button>
+              ))}
+            </div>
+
+            {/* Form */}
+            <form className="space-y-8 text-sm">
+              <div className="grid grid-cols-2 gap-6">
+                <div className="flex flex-col space-y-2">
+                  <label htmlFor="name">Name*</label>
+                  <input
+                    type="text"
+                    id="name"
+                    className="border-b border-black bg-transparent outline-none py-1"
+                    placeholder="John Doe"
+                  />
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <label htmlFor="phone">Phone number*</label>
+                  <input
+                    type="text"
+                    id="phone"
+                    className="border-b border-black bg-transparent outline-none py-1"
+                    placeholder="+91 XXXXX XXXXX"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="email">Email*</label>
+                <input
+                  type="email"
+                  id="email"
+                  className="border-b border-black bg-transparent outline-none py-1"
+                  placeholder="johndoe@email.com"
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="comment">Comment*</label>
+                <textarea
+                  id="comment"
+                  rows={4}
+                  className="border-b border-black bg-transparent outline-none py-1 resize-none"
+                  placeholder="Description"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="flex items-center space-x-2 border-b border-black mt-6 text-lg hover:opacity-70 transition"
+              >
+                <span>SEND REQUEST</span>
+                <ArrowRight className="w-4 h-4 ml-5" />
+              </button>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
       <motion.div
         className="fixed w-full bottom-0 h-12 flex justify-between text-gray-600 opacity-80 px-8"
         initial={{ transform: "translateY(100px)" }}
@@ -100,7 +202,28 @@ export default function ContactPage() {
         <p>© 2025 | Fourth Dimension</p>
         <p>All rights reserved</p>
       </motion.div>
-      {/* <Footer /> */}
+      {/* 
+      <AnimatePresence>
+        {showDialog && (
+        <motion.div
+          initial={{ x: "100%" }}
+          animate={{ x: "50%" }}
+          exit={{ x: "100%" }}
+          transition={{ duration: 0.6, ease: "easeInOut" }}
+          className="fixed top-0 right-0 h-full w-full bg-stone-400 shadow-lg z-50 p-8 overflow-auto"
+        >
+          <div className="flex justify-between items-center mb-8">
+            <h2 className="text-5xl text-black font-medium">Can we talk?</h2>
+            <button onClick={() => setShowDialog(false)}>
+              <X className="w-6 h-6 text-gray-600 hover:text-black transition" />
+            </button>
+          </div>
+          <p className="text-gray-600">
+            You can place your contact form or additional details here.
+          </p>
+        </motion.div>
+        )} 
+      </AnimatePresence> */}
     </div>
   );
 }
