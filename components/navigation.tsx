5ee5796a6d72db31dfcaae0141e9ddd4995dc5b8
pranavvraja <pranavvraja@gmail.com>
2025-07-02 17:48:52 +0530

    add: header animation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,29 +2,42 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { motion } from "motion/react";
 
 export default function Navigation() {
   const pathname = usePathname();
 
   const navItems = [
     { href: "/projects", label: "PROJECTS" },
-    { href: "/contact", label: "CONTACTS" },
+    { href: "/contact", label: "CONTACT" },
   ];
 
   return (
-    <nav className="absolute top-2 left-0 right-0 z-50">
+    <motion.nav
+      className={`absolute top-2 left-0 right-0 z-50 ${
+        pathname === "/contact" ? "text-black" : "text-white"
+      }`}
+      initial={{ transform: "translateY(-100px)" }}
+      animate={{ transform: "translateY(0px)" }}
+      transition={{ duration: 1.1, type: "decay" }}
+    >
       <div className="px-6 py-4 w-full">
         <div className="flex items-center justify-between">
-          <Link href="/" className="text-white text-4xl font-light">
-            F<sup className="text-lg">D</sup>
+          <Link href="/" className=" text-4xl font-black">
+            F
+            <sup>
+              <sup className="text-lg">D</sup>
+            </sup>
           </Link>
           <div className="hidden md:flex items-center space-x-8">
             {navItems.map((item) => (
               <Link
                 key={item.href + item.label}
                 href={item.href}
-                className={`text-white text-sm tracking-wider hover:opacity-70 hover:backdrop-blur-3xl duration-150 px-4 py-2 transition-opacity ${
-                  pathname === item.href ? "opacity-100" : "opacity-80"
+                className={`${
+                  pathname === "/contact" ? "text-black" : "text-white"
+                } text-sm tracking-wider hover:opacity-70 hover:backdrop-blur-3xl duration-150 px-4 py-2 transition-opacity ${
+                  pathname === item.href && "bg-neutral-400/40"
                 }`}
               >
                 {item.label}
@@ -33,6 +46,6 @@ export default function Navigation() {
           </div>
         </div>
       </div>
-    </nav>
+    </motion.nav>
   );
 }
