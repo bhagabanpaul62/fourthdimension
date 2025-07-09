2e682c42c8500245aa5f5024b6d940c35b9800fe
pranavvraja <pranavvraja@gmail.com>
2025-07-09 13:27:15 +0530

    add: project page dynamic

---
+++ app/api/projects/route.ts
@@ -0,0 +1,17 @@
+import { connectToDatabase } from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const client = await connectToDatabase();
+    const db = client.db("archicontrol");
+    const projects = await db.collection("categories").find({}).toArray();
+    return NextResponse.json(projects, { status: 200 });
+  } catch (err) {
+    console.error("Fetch error:", err);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
