ebd68f20d3fff5d16eb96ccd6e771e12b9a9dbfb
pranavvraja <pranavvraja@gmail.com>
2025-07-09 14:39:08 +0530

    next config image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["placehold.co", "thevirtualinstructor.com", "your-cdn.com"],
+    domains: [
+      "placehold.co",
+      "thevirtualinstructor.com",
+      "your-cdn.com",
+      "img.freepik.com",
+    ],
   },
 };
 
